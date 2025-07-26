e1bc6d3d4763bbfdfb98d63cc0c7436672343414
murali-N-tech <muralinaga826@gmail.com>
2025-07-26 14:37:08 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
+import ScrollToTop from './components/common/ScrollToTop';
 
 function App() {
   return (
